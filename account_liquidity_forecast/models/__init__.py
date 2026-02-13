3b59f979c49f5e1b9d1abd6e02e178ee420bf1e8
JordiMForgeFlow <jordi.masvidal@forgeflow.com>
2026-02-13 09:55:30 +0100

    fixup! mylva

--- account_liquidity_forecast/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import ir_actions_report
-from . import account_account
-from . import account_liquidity_forecast_planning_group
-from . import account_liquidity_forecast_planning_item
--- account_liquidity_forecast/report/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import liquidity_forecast
-from . import liquidity_forecast_xlsx
